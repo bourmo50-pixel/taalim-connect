221b7db2ef8b19c36eb541fcda0281e3a4b01c27
bourmo50-pixel <bourmo50@gmail.com>
2025-10-14 23:33:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function goTo(page) {
+  alert("الصفحة ما زالت قيد التطوير 😉");
+  // من بعد غادي نربطها بصفحات حقيقية
+}
+
+document.getElementById("language").addEventListener("change", (e) => {
+  alert("تم اختيار اللغة: " + e.target.value);
+});
